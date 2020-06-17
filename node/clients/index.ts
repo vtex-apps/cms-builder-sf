f3a24feb70cb0471be854bef3173524d6542d630
Carla Cosenza <carla.cosenza@vtex.com.br>
2020-06-17 17:35:18 -0300

    Adding install route

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -1,10 +1,15 @@
 import { IOClients } from '@vtex/api'
 
+import Billing2 from './billing'
 import Status from './status'
 
 // Extend the default IOClients implementation with our own custom clients.
 export class Clients extends IOClients {
   public get status() {
     return this.getOrSet('status', Status)
+  }
+
+  public get billing2(): Billing2 {
+    return this.getOrSet('billing2', Billing2)
   }
 }
---
+++ node/middlewares/installApp.ts
@@ -0,0 +1,51 @@
+import { parseAppId, RegistryAppVersionsList } from '@vtex/api'
+
+async function didNotFindAppResponse(ctx: Context, next: () => Promise<any>){
+  ctx.status = 200
+  ctx.body = `{"status": "fail"}`
+  await next()
+}
+
+export async function installApp(
+  ctx: Context,
+  next: () => Promise<any>
+) {
+
+  const { logger } = ctx.vtex
+  const {
+    vtex: {
+      route: { params },
+    },
+  } = ctx
+  const appID = params.code as string
+  const { name } = parseAppId(appID)
+
+  let versions: RegistryAppVersionsList = {data:[]}
+  try {
+    versions = await ctx.clients.registry.listVersionsByApp(name)
+  } catch(err) {
+    logger.warn(`Could not find previous versions of ${name}`)
+    await didNotFindAppResponse(ctx, next)
+    return
+  }
+
+  let foundApp = false
+  versions.data.forEach(element => {
+    if(element.versionIdentifier === appID) {
+      foundApp = true
+    }
+  })
+
+  if(foundApp === false){
+    await didNotFindAppResponse(ctx, next)
+    return
+  }
+
+  const installResponse = ctx.clients.billing2.installApp(appID, true, false)
+  console.log(installResponse)
+
+  ctx.status = 200
+  ctx.body = `{"status": "success"}`
+
+  await next()
+}
