5417256f45339dfe4b18afcec38402fc2ca4aa43
Mateus Pontes <mateuspo10@gmail.com>
2020-09-03 12:05:24 -0300

    Add retry option for check manifest for app

--- node/middlewares/checkPublishedApp.ts
+++ node/middlewares/checkPublishedApp.ts
@@ -1,8 +1,42 @@
-import { parseAppId } from '@vtex/api'
+import { parseAppId, Registry } from '@vtex/api'
 import { json } from 'co-body'
 
 import { InstallResponse } from '../clients/billing'
 import { returnResponseError } from '../errors/responseError'
+
+const MAX_RETRIES = 5
+const RETRY_TIMEOUT_MS = 1000
+
+const delay = (time: number) => {
+  return new Promise(resolve => setTimeout(resolve, time))
+}
+
+interface RetryCheckApp {
+  registry: Registry
+  name: string
+  version: string
+  retryCount?: number
+}
+
+const retryCheckApp = async ({
+  registry,
+  name,
+  version,
+  retryCount = 0,
+}: RetryCheckApp): Promise<any> => {
+  if (retryCount++ >= MAX_RETRIES) {
+    throw new Error('Max retries exceeded')
+  }
+
+  try {
+    await registry.getAppManifest(name, version)
+  } catch (err) {
+    console.info('Retrying', retryCount)
+    await delay(RETRY_TIMEOUT_MS)
+
+    return retryCheckApp({ registry, name, version, retryCount })
+  }
+}
 
 export async function checkPublishedApp(
   ctx: Context,
@@ -15,11 +49,11 @@ export async function checkPublishedApp(
   const { name, version } = parseAppId(appID)
 
   try {
-    await ctx.clients.registry.getAppManifest(name, version)
+    await retryCheckApp({ registry: ctx.clients.registry, name, version })
   } catch (err) {
     logger.error(`Could not find ${name} - ${err}`)
     await returnResponseError({
-      message: 'Error in build - could not find app',
+      message: `Error in build - could not find app ${name}:${version}`,
       code: 'BUILD_FAILED',
       ctx,
       next,
